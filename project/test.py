c0cbd4624da0b920e746382b4595e798f1293694
Blair VanderLugt <blair.vanderlugt@gmail.com>
2015-10-12 19:35:09 -0700

    added tests

---
+++ project/test.py
@@ -0,0 +1,32 @@
+import os
+import unittest
+
+from views import app, db
+from _config import basedir
+from models import User
+
+
+TEST_DB = 'test.db'
+
+
+class AllTests(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['WTF_CRSF_ENABLED'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+            os.path.join(basedir, TEST_DB)
+        self.app = app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+
+    def test_user_setup(self):
+        new_user = User("blair", "blair.vanderlugt@gmail.com", "blair") 
+        db.session.add(new_user)
+        db.session.commit()
+
+
+if __name__ == '__main__':
+    unittest.main()
